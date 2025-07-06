99eb0d5797dcae1c9145da951112c32ac7d6b47a
Alisher <alisher20021212@gmail.com>
2025-07-06 20:59:36 +0300

    some adjustment

--- main.js
+++ main.js
@@ -23,3 +23,19 @@ function closeMenu() {
   sideMenu.style.right = "-200px";
 }
 
+  const scriptURL = '<https://script.google.com/macros/s/AKfycbwWbRf9pUSwk0IYzxOGi22hlO3E8dYzlqY6rk8GzS3r96GhKt4nB0iYdRv0xAAqaTRO_w/exec>'
+  const form = document.forms['submit-to-google-sheet']
+  const formMessage = document.getElemetById("form-message");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        formMessage.innerHTML = "Message sent successfully";
+        setTimeout(function(){
+          formMessage.innerHtml = "";
+        },5000)
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
